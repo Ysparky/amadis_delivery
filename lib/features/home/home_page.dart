81d24604681303e1ed7221dbdec5d9bb7aed3a45
JCaldasR <jscr272000.jscr@gmail.com>
2021-01-10 01:44:24 -0500

    Mis Rutas Page

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 import 'package:amadis_delivery/core/utils/utils.dart';
@@ -14,10 +15,14 @@ class HomePage extends StatelessWidget {
         child: Container(
           height: hp(100),
           width: wp(100),
-          color: Color(0xffF6F6F6),
-          child: Center(
-            child: Image.asset(AmadisAssets.logo),
+          color: AmadisColors.backgroundColor,
+          child: IconButton(
+            onPressed: () => ExtendedNavigator.root.push(Routes.routesPage),
+            icon: Icon(Icons.refresh),
           ),
+          // child: Center(
+          //   child: Image.asset(AmadisAssets.logo),
+          // ),
         ),
       ),
     );
--- lib/features/routes/routes_page.dart
+++ lib/features/routes/routes_page.dart
@@ -1,3 +1,5 @@
+import 'package:amadis_delivery/core/utils/utils.dart';
+import 'package:amadis_delivery/core/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,7 +14,7 @@ class RoutesPage extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => RoutesViewModel()),
       ],
       child: LoadingOverlay<RoutesViewModel>(
-        child: RoutesPage(),
+        child: RoutesPageBase(),
       ),
     );
   }
@@ -22,6 +24,101 @@ class RoutesPageBase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _viewModel = Provider.of<RoutesViewModel>(context);
-    return Container();
+    return Scaffold(
+      key: _viewModel.scaffoldKey,
+      backgroundColor: AmadisColors.primaryColor,
+      appBar: CustomAppBar(headerTitle: 'Mis Rutas'),
+      body: Container(
+        color: AmadisColors.backgroundColor,
+        child: ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: hp(2)),
+          physics: BouncingScrollPhysics(),
+          itemCount: 4,
+          itemBuilder: (_, index) => RouteItem(),
+        ),
+      ),
+    );
+  }
+}
+
+class RouteItem extends StatelessWidget {
+  const RouteItem({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: wp(5),
+        vertical: hp(1),
+      ),
+      child: Card(
+        elevation: hp(0.5),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: MaterialButton(
+          onPressed: () {},
+          padding: EdgeInsets.zero,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: hp(3),
+              horizontal: wp(5),
+            ),
+            child: Row(
+              children: [
+                Icon(Icons.local_shipping),
+                SizedBox(width: wp(5)),
+                Column(
+                  children: [
+                    Text(
+                      'Ruta 1',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '01/01/2000',
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle1
+                          .copyWith(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+                SizedBox(width: wp(5)),
+                Column(
+                  children: [
+                    Text(
+                      'Num Pedidos',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1
+                          .copyWith(color: Colors.grey[600]),
+                    ),
+                    Text(
+                      '8',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Icon(Icons.navigate_next),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
